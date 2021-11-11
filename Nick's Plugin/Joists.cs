db1f13d78318d18745dd64d9d4956e1ebc4b0f0a
nshupe <nshupe@warmboard.com>
2021-11-11 07:50:53 -0800

    first working tests for the beinning of recreating the joist tool

---
+++ Nick's Plugin/Joists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick_s_Plugin
+{
+    class Joists
+    {
+        private const double defaultJoistSpcaing = 16;
+
+        public double JoistSpacing { get; set; }
+
+        public Joists()
+        {
+            JoistSpacing = defaultJoistSpcaing;
+        }
+
+        public static void runJoists()
+        {
+
+        }
+
+        private void GetSpacing()
+        {
+            JoistSpacing = 0;
+        }
+    }
+}
---
+++ Nick's Plugin/Testing/Joist Tests/JoistTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nick_s_Plugin;
+using NUnit.Framework;
+
+namespace CADTesting
+{
+    [TestFixture, Apartment(ApartmentState.STA)]
+    class JoistTests
+    {
+        [Test]
+        public void Default_Spacing()
+        {
+            Assert.That(new Joists().JoistSpacing == 16);
+
+            
+        }
+
+        [Test]
+        public void Get_Spacing_From_User()
+        {
+            Assert.That(new Joists().JoistSpacing == 16);
+
+
+        }
+    }
+}
