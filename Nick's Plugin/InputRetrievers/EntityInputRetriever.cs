690436d780c1ae01ce4f9bb1e3db251c557ee7da
nshupe <nshupe@warmboard.com>
2021-12-18 16:15:35 -0800

    Starting zone implementation. Need to rework WBObjectIdCollection to hold list of WBObjectId objects, not list of longs

---
+++ Nick's Plugin/InputRetrievers/EntityInputRetriever.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBPlugin
+{
+    class EntityInputRetriever
+    {
+        public ObjectId getUserInput(String prompt)//todo change entity to WBEntity
+        {
+            Editor ed = Active.Editor;
+
+            PromptEntityOptions promptEntityOptions = new PromptEntityOptions(prompt) { 
+                AllowObjectOnLockedLayer = true };
+
+
+            PromptEntityResult per = ed.GetEntity(promptEntityOptions);
+            if (per.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\n*Cancel*");
+                return new ObjectId();//TODO remove null
+            }
+
+            if (per.Status == PromptStatus.None)
+            {
+                return new ObjectId();//TODO
+            }
+
+            return per.ObjectId;
+        }
+    }
+}
