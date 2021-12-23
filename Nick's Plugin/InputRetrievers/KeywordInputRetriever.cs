a98da5cd0c25a43d244cd964043fde863f003807
nshupe <nshupe@warmboard.com>
2021-12-23 09:10:49 -0800

    keyword input retriever first implementation. Beginning implementation for zone shift

---
+++ Nick's Plugin/InputRetrievers/KeywordInputRetriever.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace WBPlugin
+{
+    public class KeywordInputRetriever
+    {
+        public static string GetUserInput(string prompt, params string[] keyWords)
+        {
+            Editor ed = Active.Editor;
+
+            PromptKeywordOptions promptKeywordOptions = new PromptKeywordOptions(prompt);
+
+            foreach(string keyword in keyWords)
+            {
+                promptKeywordOptions.Keywords.Add(keyword);
+            }
+
+            promptKeywordOptions.AllowNone = false;
+
+            PromptResult promptKeywordResult = ed.GetKeywords(promptKeywordOptions);
+
+            if (promptKeywordResult.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\n*Cancel*");
+                return "-99999";
+            }
+
+            return promptKeywordResult.StringResult;
+
+        }
+
+        
+    }
+}
---
+++ Nick's Plugin/Zone Tools/ZoneShift.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WBPlugin.Utilities;
+
+namespace WBPlugin.Zone_Tools
+{
+    public static class ZoneShift
+    {
+        public static bool Shift()
+        {
+            
+            WBObjectId selectedPolyLine = PolyLineInputRetriever.GetUserInput("\nSelect PolyLine to check for Zone Info: ");
+            if (selectedPolyLine.IsNull()) return false;
+            
+            Zone zone = new ZoneManager().Contains(selectedPolyLine);
+
+            if (zone == null)
+            {
+                Active.WriteMessage("\n Is not a Zone!");
+                return false;
+            }
+
+            int color = ColorManager.GetColor(selectedPolyLine);
+            StringBuilder message = new StringBuilder();
+            message.AppendFormat("\n Zone Number: {0}\nZone Color : {1}\nThermostat Type: {2}", zone.ZoneId, color, zone.Thermostat);
+            Active.WriteMessage(message.ToString());
+            return true;
+        }
+
+        
+    }
+}
